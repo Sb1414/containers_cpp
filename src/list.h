ad9790bf6db45c05b3df9fda01084bc7381f0ef8
evet <evetteis@student.21-school.ru>
2023-01-12 12:44:37 +0000

    lish.h + struct

--- src/list.h
+++ src/list.h
@@ -6,8 +6,32 @@ using namespace std;
 
 template <typename T>
 class s21_list {
+    using pointer = T*;
+    using const_reference = const T&;
+    using value_type = T;
+    using reference = T&; 
+    using const_reference = const T&;
+    using size_type = unsigned int;
+
+private:
+    struct ListNode {
+    ListNode* prev;
+    ListNode* next;
+    value_type data;
+    ListNode() : data(), prev(nullptr), next(nullptr) {}
+    };
+
     T* arr;
     int size_;
+
+public:
+    using Node = ListNode;
+
+private:
+    size_type _size;
+    Node* _front;
+    Node* _back;
+
 public:
     class Iterator;
     s21_list(int size = 1) : size_(size) {
@@ -20,15 +44,30 @@ public:
         return arr[0];
     }
 
-    Iterator begin() { return arr; }
-    Iterator end() { return arr+size_; }
+    Iterator begin() { return Iterator(_front); }
+    Iterator end() { return Iterator(_front + _size); }
+
+    // Iterator begin() { return arr; }
+    // Iterator end() { return arr+size_; }
 
     // friend ostream& operator<< (ostream& s, const s21_list<T>& size);
 
     class Iterator {
-        T* cur;
+        Node* data_;
+        pointer _node;
+        // T* cur;
     public:
-        Iterator(T* first) : cur(first) {}
+        // Iterator(T* first) : cur(first) {}
+
+        Iterator() = default;
+        explicit Iterator(pointer ptr) { _node = ptr; }
+        value_type& operator*() const { return _node->data; }
+        pointer operator->() { return _node; }
+
+        Iterator& operator++() {
+            _node = _node->next;
+            return *this;
+        }
 
         T& operator+ (int n) { return *(cur + n); }
         T& operator- (int n) { return *(cur - n); }
